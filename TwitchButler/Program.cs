a45f0667c673b01096e636be251ce7077e9f8cd8
Tobias Kattanek <tkattanek00@gmail.com>
2020-07-28 21:30:41 +0200

    Logging
    SimpleLogging class implemented for information gathering later on

---
+++ TwitchButler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Transactions;
+
+namespace TwitchButler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SimpleLogger.Log(LoggingSeverity.Ciritical, "lol xd critical");
+            SimpleLogger.Log(LoggingSeverity.Error, "lol xd error test");
+            SimpleLogger.Log(LoggingSeverity.Information, "lol xd information");
+            SimpleLogger.Log(LoggingSeverity.Warning, "lol xd warning");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+        }
+    }
+}
---
+++ TwitchButler/SimpleLogger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TwitchButler
+{
+    public enum LoggingSeverity
+    {
+        Information,
+        Warning,
+        Error,
+        Ciritical,
+        Exception
+    }
+
+    public static class SimpleLogger
+    {
+        public static void Log(LoggingSeverity severity, string message)
+        {
+            string severityPrefix = string.Format("[{0}]", severity.ToString().ToUpper());
+            SetConsoleColor(severity);
+            Console.WriteLine(string.Format("[{0}]{1} : {2}", DateTime.Now.ToString(),severityPrefix, message));
+        }
+
+        private static void SetConsoleColor(LoggingSeverity severity)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            switch(severity)
+            {
+                case LoggingSeverity.Ciritical:
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    break;
+                case LoggingSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LoggingSeverity.Exception:
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    break;
+                case LoggingSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+            }
+        }
+        
+
+    }
+}
