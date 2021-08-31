2075282fc280f1096ada6fc8509d52bc3e84b548
realTobby <tkattanek00@gmail.com>
2021-08-31 09:10:02 +0200

    changes
    connect to twitch irc, handle messages, todo: implement command sender

--- TwitchButler/Program.cs
+++ TwitchButler/Program.cs
@@ -1,22 +1,110 @@
-﻿using System;
+﻿using AsyncAwaitBestPractices;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 using System.Transactions;
 
 namespace TwitchButler
 {
+    public enum PossibleCommands
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+        Start,
+        Select,
+        A,
+        B,
+        L,
+        R
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string password = Environment.GetEnvironmentVariable("TWITCH_OAUTH");
+            string botUsername = "TwitchButler";
 
-            SimpleLogger.Log(LoggingSeverity.Ciritical, "lol xd critical");
-            SimpleLogger.Log(LoggingSeverity.Error, "lol xd error test");
-            SimpleLogger.Log(LoggingSeverity.Information, "lol xd information");
-            SimpleLogger.Log(LoggingSeverity.Warning, "lol xd warning");
+            var twitchBot = new TwitchBot(botUsername, password);
+            twitchBot.Start().SafeFireAndForget();
+            //We could .SafeFireAndForget() these two calls if we want to
+            await twitchBot.JoinChannel("actuallytobby");
+            await twitchBot.SendMessage("actuallytobby", "Hey my bot has started up");
 
-            Console.ForegroundColor = ConsoleColor.White;
+            twitchBot.OnMessage += async (sender, twitchChatMessage) =>
+            {
+                if (twitchChatMessage.Message.ToLower() == "up")
+                {
+                    twitchBot.enteredCommands.Enqueue(PossibleCommands.Up);
+                }
+
+                if (twitchChatMessage.Message.ToLower() == "down")
+                {
+                    twitchBot.enteredCommands.Enqueue(PossibleCommands.Down);
+                }
 
 
+                if (twitchChatMessage.Message.ToLower() == "left")
+                {
+                    twitchBot.enteredCommands.Enqueue(PossibleCommands.Left);
+                }
+
+
+                if (twitchChatMessage.Message.ToLower() == "right")
+                {
+                    twitchBot.enteredCommands.Enqueue(PossibleCommands.Right);
+                }
+
+
+                if (twitchChatMessage.Message.ToLower() == "a")
+                {
+                    twitchBot.enteredCommands.Enqueue(PossibleCommands.A);
+                }
+
+
+                if (twitchChatMessage.Message.ToLower() == "b")
+                {
+                    twitchBot.enteredCommands.Enqueue(PossibleCommands.B);
+                }
+
+
+                if (twitchChatMessage.Message.ToLower() == "start")
+                {
+                    twitchBot.enteredCommands.Enqueue(PossibleCommands.Start);
+                }
+
+
+                if (twitchChatMessage.Message.ToLower() == "select")
+                {
+                    twitchBot.enteredCommands.Enqueue(PossibleCommands.Select);
+                }
+
+                if (twitchChatMessage.Message.ToLower() == "l")
+                {
+                    twitchBot.enteredCommands.Enqueue(PossibleCommands.L);
+                }
+
+                if (twitchChatMessage.Message.ToLower() == "r")
+                {
+                    twitchBot.enteredCommands.Enqueue(PossibleCommands.R);
+                }
+
+
+                Console.WriteLine($"{twitchChatMessage.Sender} said '{twitchChatMessage.Message}'");
+                //Listen for !hey command
+                if (twitchChatMessage.Message.StartsWith("!hey"))
+                {
+                    await twitchBot.SendMessage(twitchChatMessage.Channel, $"Hey there {twitchChatMessage.Sender}");
+                }
+            };
+
+            await Task.Delay(-1);
         }
     }
 }
